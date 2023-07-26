435c7285554ae7f35e46e66e8239508d3518fec4
Mark Vayngrib <mvayngrib@gmail.com>
2023-07-26 16:43:37 -0400

    refactor: simplify

--- tampermonkey.js
+++ tampermonkey.js
@@ -9,132 +9,98 @@
 // @grant        none
 // ==/UserScript==
 
-(function () {
-  console.log("[GAS] Github Approve Shortcut");
+;(function () {
+  console.log('[GAS] Github Approve Shortcut')
 
-  const approvalRadio = 'form[action*="/reviews"] input[value="approve"]';
-  const approvalButton = 'button[form="pull_requests_submit_review"]';
-  const approvalComment = 'form[action*="/reviews"] #pull_request_review_body';
-  const approvalMessageSelector = "#pull_request_review_body";
-  const approvalMessageKey = "github-approve-message";
+  const combos = [
+    {
+      keys: ['alt', 'shift', 'u'],
+      message: 'utACK',
+    },
+    {
+      keys: ['alt', 'shift', 't'],
+      message: 'tACK',
+    },
+  ]
 
-  let shiftDown = false;
-  let controlDown = false;
-  let altDown = false;
-  let commandDown = false;
+  const keyToCode = {
+    shift: 16,
+    alt: 18,
+    t: 84,
+    u: 85,
+  }
+
+  const depressedKeys = {}
+
+  const approvalRadio = 'form[action*="/reviews"] input[value="approve"]'
+  const approvalButton = 'button[form="pull_requests_submit_review"]'
+  const approvalComment = 'form[action*="/reviews"] #pull_request_review_body'
+  const approvalMessageSelector = '#pull_request_review_body'
+  const approvalMessageKey = 'github-approve-message'
 
   const debug = (message, object = null) => {
-    const debugEnabled = localStorage.getItem("github-approve-shortcut-debug");
-    if (debugEnabled === "true") {
-      object ? console.debug(message, object) : console.debug(message);
+    const debugEnabled = localStorage.getItem('github-approve-shortcut-debug')
+    if (debugEnabled === 'true') {
+      object ? console.debug(message, object) : console.debug(message)
     }
-  };
+  }
 
-  document.addEventListener("keydown", (event) => {
-    debug(">>> keydown", { keyCode: event.keyCode });
-    // shift === 16
-    if (event.keyCode === 16) {
-      debug("  shift pressed");
-      shiftDown = true;
-    }
-    // control === 17
-    if (event.keyCode === 17) {
-      debug("  control pressed");
-      controlDown = true;
-    }
-    // alt === 18
-    if (event.keyCode === 18) {
-      debug("  alt pressed");
-      altDown = true;
-    }
-    // command === 93
-    if (event.keyCode === 93) {
-      debug("  command pressed");
-      commandDown = true;
-    }
+  document.addEventListener('keydown', (event) => {
+    depressedKeys[event.keyCode] = true
+    const combo = combos.find(({ keys }) =>
+      keys.every((key) => depressedKeys[keyToCode[key]])
+    )
 
-    const tDown = event.keyCode === 84;
-    const uDown = event.keyCode === 85;
+    if (combo) {
+      event.preventDefault()
+      event.stopPropagation()
+      openReviewDialog(combo.message)
+    }
+  })
 
-    if (altDown && shiftDown && uDown) {
-      debug("  shift+alt+u pressed");
-      event.preventDefault();
-      event.stopPropagation();
-      openReviewDialog("utACK");
-    }
-
-    if (altDown && shiftDown && tDown) {
-      debug("  shift+alt+t pressed");
-      event.preventDefault();
-      event.stopPropagation();
-      openReviewDialog("tACK");
-    }
-  });
-
-  document.addEventListener("keyup", (event) => {
-    debug(">>> keyUp", { keyCode: event.keyCode });
-    // shift === 16
-    if (event.keyCode === 16) {
-      debug("  shift released");
-      shiftDown = false;
-    }
-    // control === 17
-    if (event.keyCode === 17) {
-      debug("  control released");
-      controlDown = false;
-    }
-    // alt === 18
-    if (event.keyCode === 18) {
-      debug("  alt released");
-      altDown = false;
-    }
-    // command === 93
-    if (event.keyCode === 93) {
-      debug("  command released");
-      commandDown = false;
-    }
-    debug("<<<");
-  });
+  document.addEventListener('keyup', (event) => {
+    delete depressedKeys[event.keyCode]
+  })
 
   const openReviewDialog = (message) => {
-    debug(">>> openReviewDialog");
-    let newLocation = window.location.toString();
-    newLocation.includes("/files") ? newLocation : (newLocation += "/files");
-    newLocation.includes("#submit-review")
+    debug('>>> openReviewDialog')
+    let newLocation = window.location.toString()
+    newLocation.includes('/files') ? newLocation : (newLocation += '/files')
+    newLocation.includes('#submit-review')
       ? newLocation
-      : (newLocation += "#submit-review");
+      : (newLocation += '#submit-review')
 
-    debug("  ", { newLocation });
+    debug('  ', { newLocation })
 
     newLocation === window.location.toString()
       ? window.location.reload()
-      : window.location.replace(newLocation);
-    sessionStorage.setItem(approvalMessageKey, message);
-    debug("<<<");
-  };
+      : window.location.replace(newLocation)
+    sessionStorage.setItem(approvalMessageKey, message)
+    debug('<<<')
+  }
 
   const onMutation = (mutationList, observer) => {
-    debug(">>> onMutation");
-    const approvalRadioExists = document.querySelector(approvalRadio) !== null;
-    const message = sessionStorage.getItem(approvalMessageKey);
-    const triggered = !!message;
-    debug("  ", { approvalRadioExists, triggered });
+    debug('>>> onMutation')
+    const approvalRadioExists = document.querySelector(approvalRadio) !== null
+    const message = sessionStorage.getItem(approvalMessageKey)
+    const triggered = !!message
+    debug('  ', { approvalRadioExists, triggered })
 
     if (approvalRadioExists && triggered) {
-      console.log("[GAS] Approving pull request");
-      sessionStorage.removeItem(approvalMessageKey);
-      document.querySelector(approvalRadio).click();
-      document.querySelector(approvalMessageSelector).value = message;
-      document.querySelector(approvalButton).click();
+      console.log('[GAS] Approving pull request')
+      sessionStorage.removeItem(approvalMessageKey)
+      document.querySelector(approvalRadio).click()
+      document.querySelector(approvalMessageSelector).value = message
+      document.querySelector(approvalButton).click()
     }
-    debug("<<<");
-  };
-
-  if (document.getElementById("files_bucket") !== null) {
-    debug("Registering MutationObserver");
-    new MutationObserver(onMutation).observe(
-      document.getElementById("files_bucket"),
-      { attributes: true, childList: true, subtree: true }
-    );
+    debug('<<<')
   }
-})();
+
+  if (document.getElementById('files_bucket') !== null) {
+    debug('Registering MutationObserver')
+    new MutationObserver(onMutation).observe(
+      document.getElementById('files_bucket'),
+      { attributes: true, childList: true, subtree: true }
+    )
+  }
+})()
